948aa7d57a3937cc3c8d39053d210bd30603fd29
Amanda Steig <asteig@gmail.com>
2021-07-01 12:34:21 -0700

    captures working...

--- forever.py
+++ forever.py
@@ -2,14 +2,25 @@ from sh import tail
 import json
 import re 
 import utils 
+import random
 
-STATUS_QUEUED = 'queued'
+STATUS_QUEUED = "queued"
 STATUS_ACTIVE = 'active'
 STATUS_SUCCESS = 'success'
 STATUS_FAILED = 'failed'
+STATUS_COMPLETE = 'complete'
+
+print('STATUS_QUEUED', STATUS_QUEUED)
+print('STATUS_ACTIVE', STATUS_ACTIVE)
+print('STATUS_SUCCESS', STATUS_SUCCESS)
+print('STATUS_FAILED', STATUS_FAILED)
+print('STATUS_COMPLETE', STATUS_COMPLETE)
+
 
 # utils
-def colorNote(txt, color=12, bg=False, bold=False):
+def colorNote(txt, color=False, bg=False, bold=False):
+	color = color if color else random.randint(1, 125)
+	bg = bg if bg else random.randint(100, 255)
 	color_code = '\033[38;5;%dm' % color
 	bg_code = '\033[48;5;%dm' % bg if bg else ''
 	print(color_code + bg_code + txt + '\033[0m')
@@ -50,69 +61,58 @@ HISTORY = []
 # handle input from the player
 # manage initial queueing of commands...
 def handleCommandSent(packet):
-	print(packet['cmd'])
-	# add to queue
-	# TODO: double check 2 packets dont come in at once (although sessions....)
-	cmd = packet['cmd']
-	print('queueing the command.....', cmd)
-	
-	if cmd not in CMD_CAPTURES:
-		colorNote('WARNING: No captures for this command!')
 
-	# add new command to cmd queue
-	CMDS.append({
-		'cmd': cmd,
+	new_cmd = {
+		'cmd_txt': packet['cmd_txt'],
 		'received': packet['received'],
-		'completed': False,
-		'status': STATUS_QUEUED,
-		'captures': CMD_CAPTURES[cmd] if cmd in CMD_CAPTURES else False,
-		'captured': {},
-		'response': []
-	})
+		'completed': False
+	}
+	
+	if new_cmd['cmd_txt'] in CMD_CAPTURES:
+		queueCommand(new_cmd)
+		
+# add a command to global CMDS queue
+def queueCommand(cmd):
+	print('queueCommand', cmd['cmd_txt'])
+	cmd['status'] = STATUS_QUEUED
+	cmd['captures'] = CMD_CAPTURES[cmd['cmd_txt']]
+	cmd['captured'] = {}
+	cmd['response'] = []
+	CMDS.append(cmd)
 
 def handleTxtReceived(packet):
+
 	# get line text
 	sText = packet['txt']
 	
-	active_command = CMDS[0] if CMDS else False
-	
-	# get active command capture
-	if active_command:
-		
-		# did the command fail?
-		result = re.match(r'(^What\?$|^Try something else.$|^That doesn\'t work.$)', sText)
-		if result:
-			colorNote('COMMAND FAILED: ' + CMDS[0]['cmd'])
-			active_command['status'] = STATUS_FAILED
-			active_command['completed'] = packet['received']
-			HISTORY.append(active_command)
-			CMDS.pop(0)
-			return False
-		
-		# if the command already started...
-		active_command['response'].append(sText)
-		
-		stop_capture = active_command['captures'][-1] if active_command['captures'] else False
-		
-		# is this the last capture?
-		if stop_capture and re.match(stop_capture, sText):
-			colorNote('ENDING! FULL RESPONSE:', 12, 1)
+	if CMDS:
 
-			active_command['status'] = STATUS_SUCCESS
-			active_command['completed'] = packet['received']
+		start_capture = CMDS[0]['captures'][0] if CMDS[0]['captures'] else False
+
+		# is this the start? (assume it is not also the end)
+		if start_capture and re.match(start_capture, sText):
+			CMDS[0]['status'] = STATUS_ACTIVE
+		
+		# is this the end?
+		if CMDS[0]['status'] == STATUS_ACTIVE:
+			CMDS[0]['response'].append(sText)
 			
-			# get captures:
-			captured = getCaptured(active_command['captures'], active_command['response'])
-			print('CAPTURED!!!!!', captured)
+			stop_capture = CMDS[0]['captures'][-1]
 			
-			HISTORY.append(active_command)
-			CMDS.pop(0)
+			if stop_capture and re.match(stop_capture, sText):
+				CMDS[0]['captured'] = getCaptured(CMDS[0]['captures'], CMDS[0]['response'])
+				CMDS[0]['status'] = STATUS_SUCCESS
+				CMDS[0]['completed'] = packet['received']
+				HISTORY.append(CMDS[0])
+				
+				colorNote('CAPTURED!')
+				for k in CMDS[0]['captured']:
+					print(k.upper()+':', CMDS[0]['captured'][k])
+				
+				CMDS.pop(0)
 
 
 def getCaptured(captures, lines):
-	
-	print('getCaptured:')
-	[print(line) for line in lines]
 	
 	all_captured = {}
 	
@@ -123,8 +123,6 @@ def getCaptured(captures, lines):
 				groups = result.groupdict()
 				captured = {k:v for k,v in groups.items() if v is not None}
 				all_captured.update(captured)
-				if captures:
-					captures.pop(0)
 	
 	return all_captured
 
@@ -137,11 +135,10 @@ def START():
 	
 	# runs forever
 	while True:
-		for line in tail('-f', '/home/zaya/Apps/MUSHclient/x/sync.in', _iter=True):
+		for packet in tail('-f', '/home/zaya/Apps/MUSHclient/x/sync.in', _iter=True):
 
-			data = json.loads(line)
-
-			if 'cmd' in data:
+			data = json.loads(packet)
+			if 'cmd_txt' in data:
 				handleCommandSent(data)
 				
 			if 'txt' in data:
